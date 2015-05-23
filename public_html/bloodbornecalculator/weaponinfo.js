426d20ed0d82d2c5779cb4365b36937bec6a5f0a
cdfranz2 <cdfranz2@illinois.edu>
2015-05-23 10:48:04 -0500

    Add Bloodborne calculator

---
+++ public_html/bloodbornecalculator/weaponinfo.js
@@ -0,0 +1,30 @@
+// values denote the weapon's stats for the upgrade level corresponding to its index
+var weaponInfo = 
+{
+	'Saw Cleaver': [
+		{baseatk: 90, strscl: 'D', skiscl: 'E', bldscl: null, arcscl: 'D'},
+		{baseatk: 99, strscl: 'D', skiscl: 'E', bldscl: null, arcscl: 'D'},
+		{baseatk: 108, strscl: 'D', skiscl: 'E', bldscl: null, arcscl: 'D'},
+		{baseatk: 117, strscl: 'D', skiscl: 'E', bldscl: null, arcscl: 'D'},
+		{baseatk: 126, strscl: 'D', skiscl: 'E', bldscl: null, arcscl: 'D'},
+		{baseatk: 135, strscl: 'D', skiscl: 'D', bldscl: null, arcscl: 'D'},
+		{baseatk: 144, strscl: 'D', skiscl: 'D', bldscl: null, arcscl: 'D'},
+		{baseatk: 153, strscl: 'D', skiscl: 'D', bldscl: null, arcscl: 'D'},
+		{baseatk: 162, strscl: 'C', skiscl: 'D', bldscl: null, arcscl: 'C'},
+		{baseatk: 171, strscl: 'C', skiscl: 'D', bldscl: null, arcscl: 'C'},
+		{baseatk: 180, strscl: 'C', skiscl: 'D', bldscl: null, arcscl: 'C'}
+	],
+	'Threaded Cane': [
+		{baseatk: 78, strscl: 'E', skiscl: 'C', bldscl: null, arcscl: 'D'},
+		{baseatk: 85, strscl: 'E', skiscl: 'C', bldscl: null, arcscl: 'D'},
+		{baseatk: 92, strscl: 'E', skiscl: 'B', bldscl: null, arcscl: 'D'},
+		{baseatk: 99, strscl: 'E', skiscl: 'B', bldscl: null, arcscl: 'D'},
+		{baseatk: 106, strscl: 'E', skiscl: 'B', bldscl: null, arcscl: 'D'},
+		{baseatk: 113, strscl: 'E', skiscl: 'B', bldscl: null, arcscl: 'C'},
+		{baseatk: 120, strscl: 'E', skiscl: 'B', bldscl: null, arcscl: 'C'},
+		{baseatk: 127, strscl: 'E', skiscl: 'B', bldscl: null, arcscl: 'C'},
+		{baseatk: 134, strscl: 'E', skiscl: 'B', bldscl: null, arcscl: 'C'},
+		{baseatk: 141, strscl: 'E', skiscl: 'A', bldscl: null, arcscl: 'C'},
+		{baseatk: 156, strscl: 'E', skiscl: 'A', bldscl: null, arcscl: 'B'}
+	]
+}
